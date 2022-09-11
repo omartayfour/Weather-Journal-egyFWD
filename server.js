826a804bcdceeab2495638f30f337cb65db451d3
omartayfour <omartayfourr@gmail.com>
2022-09-11 23:42:20 +0200

    initial boiler code

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+const bodyParser = requre('bodyParser');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+function listening() {
+    console.log(`running on localhost: ${port}`);
+};
